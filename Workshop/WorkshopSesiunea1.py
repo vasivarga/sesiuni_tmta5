c1650cd5473809f78e9879b12af6bc41708923bc
MaximDigitalKL <maximrazvan1990@gmail.com>
2023-07-23 18:25:51 +0300

    workshop2

--- Workshop/WorkshopSesiunea1.py
+++ Workshop/WorkshopSesiunea1.py
@@ -33,16 +33,16 @@ print(media_la_bac)
 
 ''' Type Casting poate fi folosit in convertirea din string in integer, dar si din integer in string'''
 #exemplul 1
-var1 = "2"
-print(type(var1))
-var1 = int(var1)
-print(type(var1))
-#exemplul 2
-var2 = 3
-print(type(var2))
-var2 = str(var2)
-print(type(var2))
-print(int(elev))
+# var1 = "2"
+# print(type(var1))
+# var1 = int(var1)
+# print(type(var1))
+# #exemplul 2
+# var2 = 3
+# print(type(var2))
+# var2 = str(var2)
+# print(type(var2))
+# print(int(elev))
 
 #Ex_6 - Incearca sa convertesti variabila boolean la int folosind type casting si observa rezultatele
 
@@ -76,7 +76,7 @@ nume = "Tutuca"
 prenume = "Petre"
 '''
 cal = 1   "000 110 001"
-          " c   a   l "  
+          " c   a   l "
 Cal = 1   "010 110 001"
           " C   a   l
 declarare si initializare de variabile diferite
@@ -237,5 +237,185 @@ print(noul_cuvant)
 # Ex_5 - citeste un user de la tastatura, citeste o parola. Afiseaza: 'Parola pt user x este ***** si are x caractere
 user = "maxim"
 parola = "maxsadfgherstdyguih"
-print(f'Parola pentru user {user} este ***** si are {len(parola)} caractere')
 print(f'Parola pentru user {user} este {"*"*len(parola)} si are {len(parola)} caractere')
+print(f'Parola pentru user {user} este ***** si are {len(parola)} caractere')
+
+
+#Ex_1 Explica cu cuvintele tale in cadrul unui comentariu cum functioneaza un if else
+
+'''
+Structura if else, putem sa o consideram ca pe o intersectie.
+Daca conditia este adevarata mergem la dreapta si executam codul
+Daca conditaeste falsa mergem la stanga si executam codul/ skip
+'''
+
+#Ex_2 Verifica si afiseaza daca x este numar natural sau nu (un numar se considera natural daca este numar intreg mai mare ca 0)
+x = 7
+
+if x >= 0 and type(x) == int:
+    print(f'{x} este numar natural')
+else:
+    print(f'Numarul {x} nu este numar natural')
+
+#Ex_3 Verifica si afiseaza daca x este numar pozitiv, negativ sau neutru
+
+if int(x) > 0:
+    print("numar pozitiv")
+elif int(x) < 0:
+    print("numar negativ")
+else:
+    print("neutru")
+
+
+#Ex_4 Verifica si afiseaza daca x este intre -2 si 13 (incluzand captele de interval).
+
+# if x >= -2 and x <= 13:
+#     print("numarul este inclus in intervalul -2 13")
+# else:
+#     print("nu este inclus in interval -2 13")
+
+
+
+#Ex_5 Verifica daca x NU este intre 5 si 27, excluzand capetele de interval. (a se folosi ‘not’)
+x = 7
+
+if not(x >5 and x < 27):   #if not(x>5)  and not(x<27)
+    print("numarul nu este in interval")
+else:
+    print("numarul este in interval")
+
+# if not x > 5 and not x < 27:       # expresia contine o dubla negatie - Not >5 si not < 27 || si Not < 27
+#     print("nu este in interval")
+# else:
+#     print("este in interval")
+
+
+#Ex_6 Declara o noua variabila y de tip int si apoi verifica si afiseaza daca x si y sunt egale, daca nu, afiseaza care din ele este mai mare
+x = 8
+y = 8
+if x > y:
+    print(f'{x} este mai mare ca {y}')
+elif y > x :
+    print(f'{y} este mai mare ca {x}')
+else:
+    print("numerele sunt egale")
+
+#Ex_7 Presupunand ca x, y, z (toate de tip int) reprezinta laturile unui triunghi, afiseaza daca triunghiul este isoscel (doua laturi sunt egale), echilateral (toate laturile sunt egale) sau oarecare (nicio latura nu e egala).
+x = 3
+y = 6
+z = 9
+if x == y and x == z and y == z:
+    print("triunghi echilateral")
+elif x == y or x == z or y == z:
+    print("triunghi isoscel")
+else:
+    print("triunghi oarecare")
+
+#Ex_8 Citeste o litera de la tastatura apoi verifica si afiseaza daca este vocala sau nu. Atentie! Trebuie sa evaluati si cazurile uppercase si cazurile lowercase.
+litera = "E"  #input(str("introduceti litera = "))
+if litera.upper() == "A" or litera.upper() == "E" or litera.upper() == "I" or litera.upper() == "O" or litera.upper() == "U":
+    print("litera este vocala")
+elif litera.isdigit():
+    print("caracterul introdus nu este litera")
+else:
+    print("litera este consoana")
+
+lita_vocale = ["A", "E", "I", "O", "U"]
+if litera.upper() in lita_vocale:
+    print("litera este vocala")
+else:
+    print("litera este consoana")
+
+'''
+Calculare pret discount
+
+Dacă un client are peste 65 de ani, atunci i se va oferi o reducere de 15%.
+În caz contrar, dacă clientul nu are peste 65 de ani, dacă persoana călătorește cu cel puțin un copil va avea o reducere de 10%
+Atat pentru seniori cat si pentru non- seniori se va aplica o reducere suplimentara de 10% daca vor calatori in timpul iernii.
+De asemenea, atât pentru seniori, cât și pentru non seniori se va aplica o taxă de lux de 3% dacă vor călători în clasa I (în orice sezon) sau 1% taxă de gestiune în caz contrar.
+'''
+varsta_client = 66
+discount = 0
+copii = False
+sezon = "iarna"
+clasa = "2"
+pret = 100
+
+if varsta_client > 65:
+    discount = 0.15
+elif copii == True:
+    discount = 0.1
+if sezon == "iarna":
+    discount += 0.1  # discount = discount + 0.1
+if clasa == "1":
+    tax = 0.03
+else:
+    tax = 0.01
+pret_calatorie = pret - (pret*discount) + (pret*tax)
+print(f'Pretul excursiei este: {pret_calatorie}')
+
+
+'''
+Calculare discount seller
+
+Compania X vinde mărfuri la punctele de vânzare cu ridicata și cu amănuntul. 
+Clienții angro primesc o reducere de două procente la toate comenzile. 
+De asemenea, compania încurajează atât clienții angro, cât și clienții cu amănuntul
+să plătească ramburs la livrare, oferind o reducere de două procente pentru această 
+metodă de plată. Încă o reducere de două procente este acordată pentru comenzile de 
+50 sau mai multe unități. 
+
+'''
+client_angro = True  #False
+reducere = 0
+cantitate = 50
+ramburs = True  #False
+pret = 100
+if client_angro:
+    reducere = 0.02
+if ramburs:
+    reducere += 0.02
+if cantitate >= 50:
+    reducere += 0.02
+pret_final = pret - (pret*reducere)
+print(f'Pretul total al comenzii este {pret_final}')
+
+'''
+Introduceti un nume de film de la tastatura si evaluati daca numele acelui film este 
+numele filmului vostru preferat. Daca da, atunci printati pe ecran: 
+“Acesta este filmul meu preferat”. In caz contrar printati pe ecran: 
+“Din pacate nu este filmul meu preferat”
+'''
+film = "Terminator"   #input("Introduceti filmul: ")
+film_preferat = "Titanic"
+if film == film_preferat:
+    print("Acesta este filmul meu preferat")
+else:
+    print("Din pacate nu este filmul meu preferat")
+
+
+"""
+Aveti la dispozitie urmatorul database url:
+jdbc:mysql://mysql.db.server:3306/my_database?useSSL=false&serverTimezone=UTC
+host = jdbc:mysql://
+baza_de_date = mysql.db.server
+:3306 = portul de access
+/my_database?useSSL=false&serverTimezone=UTC = 
+
+SELECT "cereneala" FROM "nume tabel"
+WHERE filter and filter
+ORDER by ASC sau DESC
+
+Extrageti din acest text numele bazei de date: mysql.db.server
+Folositi un if statement pentru a evalua daca numele bazei de date 
+este cel corect (presupunand ca extrageti url-ul dintr-un sistem extern si 
+nu stiti care este acesta)
+"""
+data_base_link = "jdbc:mysql://mysql.db.server:3306/my_database?useSSL=false&serverTimezone=UTC"
+expected_data_base_name = "mysql.db.server"
+link_without_host = data_base_link[data_base_link.index("/")+2: len(data_base_link)]
+data_base_name = link_without_host[0:link_without_host.index(":")]
+if data_base_name == expected_data_base_name:
+    print("baza de date este corecta")
+else:
+    print("baza de date nu este corecta")
---
+++ Workshop/WorkshopSesiunea2.py
@@ -0,0 +1,352 @@
+# 1. Declarati o lista cu elemente multitype
+
+lista = ["Ana", "Anton", 2, 45.6, True]  # se acceseaza pe baza de index
+print(lista)  # accepta elemente duplicate
+# se poate modifica structura (adauga si sterge elemente)
+
+
+# 2. Declarati o lista goala
+
+lista_goala = list()
+
+"""
+set = set()
+print(type(set))
+dictionar = dict()
+print(type(dictionar))
+"""
+
+
+# 3. Accesati orice element din lista pe baza de index
+
+lista = ["Ana", "Anton", 2, 45.6, True]
+print(lista[2])
+
+
+# 4. Accesati pozitia unui element din lista pe baza functiei index()
+print(lista.index("Anton"))
+
+
+# 5. Adaugati elemente in lista atat cu functia append() cat si cu functia insert(). Observati rezultatele
+lista.append("cascaval")
+print(lista)
+lista.insert(1, "Marcel")
+print(lista)
+
+# 6. Stergeti elemente din lista atat cu functia pop() cat si cu functia remove(). Observati rezultatele
+lista.pop()
+print(lista)
+lista.remove("Marcel")
+print(lista)
+
+# 7. Numarati elementele dintr-o lista folosind functia len()
+print(f"Lista are {len(lista)} elemente")
+
+
+# 8. Numarati de cate ori apare un anumit element in lista folosind functia count()
+
+print(lista.count("Ana"))
+lista.append("Ana")
+print(lista.count("Ana"))
+lista.pop()
+
+# 9. Uniti doua liste folosind functia extend()
+lista2 = [1, 4, 6]
+# lista = lista + lista2
+# lista2 = lista + lista2
+print(lista2)
+# lista3 = lista + lista2
+# lista3V2 = lista.extend(lista2)  NO BUENO
+# print(lista3)
+lista.extend(lista2)
+print(lista)
+# print(f'lista extinsa {lista.extend(lista2)}')  NO BUENO
+
+
+# 10. Sortati lista folosind functia sort()
+lista_mea = [3, 5, 9, 7]
+lista_mea.sort()
+print(lista_mea)
+
+# 11. Inversati continutul listei folosind functia reverse()
+lista_mea.reverse()
+print(lista_mea)
+
+# 12. Stergeti toate elementele din lista folosind functia clear()
+lista.clear()
+print(lista)
+
+# 13. Parcurgeti o lista si printati toate elementele din aceasta folosind pe rand for, while si foreach
+
+# for stim numarul de pasi, ai o limita
+
+for i in range(0, len(lista_mea)):
+    print(lista_mea[i])
+
+print("#############################")
+# while   merge pana o conditie este indeplinita (nu stim numarul de pasi)
+i = 0
+while i < len(lista_mea):
+    print(lista_mea[i])
+    i = i + 1
+print("#############################")
+
+# for "each"
+
+for element in lista_mea:
+    print(element)
+
+print("#############################")
+
+# Bonus 1: Sortati o lista folosind algoritmul de sortare prin metoda bulelor
+bule = [9, 5, 6, 1, 2, 13, 29, 17, 36, 0]
+switch = ""
+j = 1
+while j < len(bule):
+    for i in range(0, len(bule)-1):
+        if bule[i] > bule[i+1]:         # daca 9 (pozitia 0) > 5 (pozitia 1)
+            switch = bule[i]            #switch = 9 (pozitia 0)  -> actual pozitia 0 = null
+            bule[i] = bule[i+1]         #pozitia 0 = 5 (pozitia 1)  -> actual pozitia 1  devine null
+            bule[i+1] = switch          #pozitia 1 = 9 (switch)
+    j = j + 1
+print(bule)
+#bule = [5, 6, 1, 2, 9, 13, 17, 29, 0, 36]  # nu este sorata complet mai trebuie iterat cel putin o data
+
+#bule = [9, 5, 6, 1, 2, 13, 29, 17, 36, 0]  #index 0
+        # [5 9 6 1 2 13 29 17 36 0]  #index 1
+        # [5 6 9 1 2 13 29 17 36 0]  #index 2
+        # [5 6 1 9 2 13 29 17 36 0] #index 3
+        # [5 9 1 2 9 13 29 17 36 0] #index 4
+        # [5 9 1 2 9 13 29 17 36 0]  #index 5
+        # [5 9 1 2 9 13 29 17 36 0]   #index 6
+        # [5 9 1 2 3 13 17 29 36 0]   #index 7
+        # [5 9 1 2 3 13 17 29 36 0]      #index 8
+        # [5 9 1 2 3 13 17 29 0 36] #index 9
+        # [5 9 1 2 3 13 17 29 0 36] finala
+# for i in range(0, len(bule)-1):
+#     if bule[i] > bule[i+1]:         # daca 9 (pozitia 0) > 5 (pozitia 1)
+#         switch = bule[i]            #switch = 9 (pozitia 0)  -> actual pozitia 0 = null
+#         bule[i] = bule[i+1]         #pozitia 0 = 5 (pozitia 1)  -> actual pozitia 1  devine null
+#         bule[i+1] = switch          #pozitia 1 = 9 (switch)
+#
+# print(bule)
+
+#
+#
+#
+#
+#
+# # Bonus 2: Incercati sa printati pe ecran pe cei 101 dalmatieni.
+# La fiecare dalmatian veti printa pe ecran urmatorul text:
+# “Dalmatianul curent se afla in pozitia “i””.
+# Atentie, ghilimelele vor trebui tratate folosind caracterul escape
+
+i = 1
+while i <= 101:
+    print(f'Dalmatianul curent se afla in pozitia \"{i}\"')
+    i = i + 1
+
+
+# 1. Declarati un tuplu        # are structura fixa si ca numar si ca valoare
+# se poate accesa prin index
+tuplu = (5, 6, 10, 7)
+
+# 2. Declarati un tuplu gol
+tuplu_gol = tuple()
+
+# 3. Accesati orice element din tuplu pe baza de index
+print(tuplu[2])
+
+# 4. Accesati pozitia unui element din lista pe baza functiei index()
+print(tuplu.index(5))
+
+# 5. Folositi functia count() pentru a numara de cate ori apare un element in tuplu
+print(tuplu.count(6))
+
+# 6. Folositi functia index() pentru a verifica pozitia la care se afla un
+# anumit element in tuplu.
+print(tuplu.index(7))
+
+# 7. Incercati sa modificati un element din tuplu si observati rezultatele
+# tuplu.remove(10)
+# tuplu.append(15)
+
+
+# 1. Declarati un set
+
+setul_meu = {1, 4, 7, 2, 5}  # nu accepta duplicate
+print(setul_meu)  # accesare nu se print index
+# poate fi modificat
+# 2. Declarati un set gol
+set_gol = set()
+
+# 3. Adaugati un element nou in set folosind functia add()
+setul_meu.add(12)
+print(setul_meu)
+setul_meu.add(0)
+print(setul_meu)
+
+# 4. Stergeti un element din set folosind functia pop() si respectiv functia remove(). Observati rezultatele
+setul_meu.pop()
+print(setul_meu)
+setul_meu.remove(5)
+print(setul_meu)
+
+# 5. Verificati daca un set este o subdiviziune a unui alt set (subset)
+set_2 = {2, 4}
+if set_2.issubset(setul_meu):
+    print("este subset")
+
+# 6. Verificati daca un set contine toate elementele dintr-un alt set + alte elemente (superset)
+
+if setul_meu.issuperset(set_2):
+    print("este supraset")
+
+# 7. Verificati care sunt elementele comune intre doua seturi (intersection)
+
+print(setul_meu.intersection(set_2))
+print(set_2.intersection(setul_meu))
+
+# 8. Verificati diferenta intre doua seturi cu functia difference()
+print(setul_meu.difference(set_2))
+
+# 9. Stergeti toate elementele dintr-un set folosind functia clear()
+setul_meu.clear()
+print(setul_meu)
+
+
+# 1. Declarati un dictionar       # functioneaza pe principiul cheie: valoare
+dictionarul_meu = {  # key este unica
+    "elev": "premiant",  # value poate avea duplicate
+    "nota": 9.50,
+    "promovat": True,
+}
+print(dictionarul_meu)
+
+# 2. Declarati un dictionar gol
+dict_gol = dict()
+
+# 3. Adaugati un element nou in dictionar folosind functia update()
+# si respectiv pe baza de cheie
+element_nou = {"tip_examen": "BAC"}
+dictionarul_meu.update(element_nou)  # adaugare element cu functia update()
+print(dictionarul_meu)
+dictionarul_meu.update({"elev": "olimpic"})
+print(dictionarul_meu)
+dictionarul_meu.update(element_nou)
+print(dictionarul_meu)
+dictionarul_meu.update({"varsta": "18 ani"})
+print(dictionarul_meu)
+dictionarul_meu["nume"] = "Tutuca Petre"  # adaugare pe baza de cheie
+print(dictionarul_meu)
+
+
+# 4. Extrageti un element din dictionar folosind metoda get() si respectiv pe baza de cheie
+print(dictionarul_meu.get("nume"))
+print(dictionarul_meu["nume"])
+
+# 5. Stergeti un element din dictionar folosind functia pop() si respectiv functia popitem().
+# Observati rezultatele
+dictionarul_meu.pop("tip_examen")
+print(dictionarul_meu)
+dictionarul_meu.popitem()
+print(dictionarul_meu)
+
+# 6. Extrageti pe rand toate cheile, apoi toate valorile, si apoi toate item-urile
+# din dictionar
+print(f"keys: {dictionarul_meu.keys()}")
+print(f"values: {dictionarul_meu.values()}")
+print(f"perechi: {dictionarul_meu.items()}")
+
+
+# 7. Stergeti toate valorile din dictionar folosind metoda clear()
+dictionarul_meu.clear()
+print(dictionarul_meu)
+
+print("#############################################")
+
+fotbalisti_pe_echipe = {
+    "Barcelona": {
+        "Dica": {
+            "Nume complet": "Nicolae Dica",
+            "Varsta": 45,
+            "Numar Tricou": 10
+        },
+        "Banel": {
+            "Nume complet": "Banel Nicolita",
+            "Varsta": 47,
+            "Numar Tricou": 3
+        },
+        "Dukadam": {
+            "Nume complet": "Helmut Dukadam",
+            "Varsta": 65,
+            "Numar Tricou": 7
+        },
+    },
+    "Real-Madrid": {
+        "mutu": {
+            "Nume complet": "mutu",
+            "Varsta": 45,
+            "Numar Tricou": 10
+        },
+        "radoi": {
+            "Nume complet": "radoi",
+            "Varsta": 47,
+            "Numar Tricou": 3
+        },
+        "chivu": {
+            "Nume complet": "chivu",
+            "Varsta": 65,
+            "Numar Tricou": 7
+        },
+        "marian": {
+            "Nume complet": "marian",
+            "Varsta": 65,
+            "Numar Tricou": 7,
+            "accidentat": True,
+        },
+    }
+}
+#Printati pe ecran numarul de pe tricou al oricarui jucator doriti
+print(fotbalisti_pe_echipe["Barcelona"]["Banel"]["Numar Tricou"])
+
+#Folositi functia pop pentru a scoate orice jucator din dictionar
+#fotbalisti_pe_echipe["Barcelona"].pop("Banel")
+#print(fotbalisti_pe_echipe)
+''''
+fotbalisti_pe_echipe = {
+    "Barcelona": {
+        "Dica": "nume",
+        "Banel": "nume",
+        "Dukadam": "nume"},
+    "Real Madrid": {
+            "radoi": "nume",
+            "mutu": "nume",
+            "chivu": "nume"}
+}'''
+
+#Printati pe ecran detaliile despre fiecare jucator astfel incat sa obtineti urmatorul rezultat:
+
+
+'''
+La echipa Barcelona joaca jucatorul:
+Detalii jucator - Nume Complet: Nicolae Dica, Detalii jucator - Varsta:45, Detalii jucator - Numar Tricou:10
+
+La echipa Barcelona joaca jucatorul:
+Detalii jucator - Nume Complet: Banel Nicolita, Detalii jucator - Varsta:47, Detalii jucator - Numar Tricou:
+
+La echipa Barcelona joaca jucatorul:
+Detalii jucator - Nume Complet: Dukadam, Detalii jucator - Varsta:45, Detalii jucator - Numar Tricou:7
+
+'''
+#for element in list_of_elements  for each
+
+for key, value in fotbalisti_pe_echipe.items():  #aici accesam echipele (cheia Barcelona...Valoare jucatorii( dica banel dukadam)
+#barcelona, jucatorii
+    for key_interioara, value_interioara in value.items():
+        print(f'La echipa {key} joaca jucatorul: ')
+        for key_jucator, value_jucator in value_interioara.items():
+            print("Detalii jucator -" f' {key_jucator}:{value_jucator}', end=", ")
+            # end= incheie fiecare iteratie cu elementul dorit
+        print("\n")       #punem un rand gol (Enter in Word)
+
